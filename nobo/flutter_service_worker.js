dafe4c868342429c11653c66f1e3548a7730a37b
Jemal Rashidi <jmsrsd@gmail.com>
2021-12-22 01:39:59 +0700

    update

--- nobo/flutter_service_worker.js
+++ nobo/flutter_service_worker.js
@@ -17,9 +17,9 @@ const RESOURCES = {
 "icons/Icon-512.png": "96e752610906ba2a93c65f8abe1645f1",
 "icons/Icon-maskable-192.png": "c457ef57daa1d16f64b27b786ec2ea3c",
 "icons/Icon-maskable-512.png": "301a7604d45b3e739efc881eb04896ea",
-"index.html": "64f345e53b74c021c8f877502ee38352",
-"/": "64f345e53b74c021c8f877502ee38352",
-"main.dart.js": "6abe09a77101c5556a5990b8a5eb8d0a",
+"index.html": "8cf8d765f02704a2d9520dc9ebf18477",
+"/": "8cf8d765f02704a2d9520dc9ebf18477",
+"main.dart.js": "5758081c4a13d9a654142520aac3f7aa",
 "manifest.json": "4960911ebec2f9b369807430f1554501",
 "version.json": "b5815471b649f9de72310dabfff543e8"
 };
